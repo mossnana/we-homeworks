63d5a5e3040d646275716542c008936bd2876d83
Permpoon Chaowanaphunphon <38369861+mossnana@users.noreply.github.com>
2019-02-09 22:26:48 +0700

    Update DataModel.kt

--- week4/DataModel.kt
+++ week4/DataModel.kt
@@ -1,16 +1,21 @@
 package com.example.mycomic
 
 class DataModel {
-
+    // Key Variables Declaring
     var title: String?=null
     var desc: String?=null
+    // Comic Cover
     var photos: String?=null
     var files: String?=null
-    var key:String?=null
-    var photo:String?=null
-
+    var key: String?=null
+    // Comic Photo Set
+    var photo: String?=null
+    
+    // Constructor with null argument
+    // use when no data received
     constructor()
-
+    
+    // Constructor with received firebase 
     constructor(title: String?, desc: String?, photos: String?, files: String?, key: String?, photo: String?) {
         this.title = title
         this.desc = desc
@@ -19,7 +24,9 @@ class DataModel {
         this.key = key
         this.photo = photo
     }
-
+    
+    // Map Received Data to HashMap
+    // Hashmap is easy to input data than list
     fun toMap(): Map<String, Any> {
         val result = HashMap<String, Any>()
         result.put("title", title!!)
@@ -30,6 +37,4 @@ class DataModel {
         result.put("photo", photo!!)
         return result
     }
-
-
 }
