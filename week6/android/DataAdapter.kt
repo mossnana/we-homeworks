189f9b647b8f0e203e545ba3dc5cb026236c71b4
Permpoon Chaowanaphunphon <38369861+mossnana@users.noreply.github.com>
2019-03-10 23:15:40 +0700

    Update DataAdapter.kt

--- week6/android/DataAdapter.kt
+++ week6/android/DataAdapter.kt
@@ -16,26 +16,24 @@ import com.squareup.picasso.Picasso
 import java.util.ArrayList
 
 class DataAdapter(val dataModelList: List<DataModel>) : RecyclerView.Adapter<ViewHolder>() {
-
-
+    // ตัวแปร mylist ใช้ในการ map ข้อมูลที่ได้จาก Firebase ส่งไปหน้า DataModel.kt
     val mylist = ArrayList<String>()
-
+    
+    // ViewHolder Methods
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(p0.context).inflate(R.layout.model,p0,false))
     }
-
+    
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        // dataModel เอาไว้ใส่ List ของ ViewHolder ตัวต่างๆ
         val dataModel = dataModelList[p1]
-        p0.textTitle.text = dataModel.title
-
-
-
+        // สร้างตัวแปรติดต่อกับฐานข้อมูล
         var databaseReference = FirebaseDatabase.getInstance().reference
-
-
+        // ???
+        p0.textTitle.text = dataModel.title
+        // Firebase Query Method
         databaseReference.child("stream/channel").addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-
                 Log.d(TAG, "Count= " + dataSnapshot.childrenCount)
                 for (childDataSnapshot in dataSnapshot.children) {
                     Log.d(TAG, "snapshot= " + childDataSnapshot.key!!)
@@ -43,35 +41,35 @@ class DataAdapter(val dataModelList: List<DataModel>) : RecyclerView.Adapter<Vie
                 }
             }
             override fun onCancelled(databaseError: DatabaseError) {
+                // None
             }
         })
-
-
+        
+        // ใช้ Picasso เก็บแคชของ viewholder
         Picasso.with(p0.itemView.context).load(dataModel.photos)
             .error(R.mipmap.ic_launcher)
             .placeholder(R.mipmap.ic_launcher)
             .into(p0.imageView)
-
+        
+        // กำหนด event ให้ คลิกที่ view holder ตัวนั้นแล้วไปหน้าอื่นแล้วส่งข้อมูลไปด้วย
         p0.imageView.setOnClickListener(View.OnClickListener { v ->
+            // ???
             val content = dataModel.content
-
             val readActivity = Intent(v.context, WatchActivity::class.java)
             readActivity.putExtra("source", content)
             v.context.startActivity(readActivity)
         })
-
     }
-
+    
     override fun getItemCount(): Int {
         return dataModelList.size
     }
-
 }
 
+// ViewHolder Class for Imprementation
 class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     var textTitle: TextView = itemView.findViewById(R.id.title)
     var imageView: ImageView
-
     init {
         imageView = itemView.findViewById(R.id.thumbnail)
     }
