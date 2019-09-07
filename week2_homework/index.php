b58e0466018beb088b287eedca775d17da7ec6ad
Permpoon Chaowanaphunphon <38369861+mossnana@users.noreply.github.com>
2019-09-08 00:03:44 +0700

    Basic PHP Unit Test

---
+++ week2_homework/index.php
@@ -0,0 +1,48 @@
+﻿<html>
+<head>
+<title>Ninety Surprise !!!</title>
+</head>
+<body>
+<form action="index.php" method="post">
+<label for="number1">Number 1</label>
+<input type="text" name="number1" placeholder="Enter number 1" />
+<br/>
+<label for="number2">Number 2</label>
+<input type="text" name="number2" placeholder="Enter number 2" />
+<br/>
+<button type="submit">Summary</button>
+</form>
+<?php
+
+function sum($num1, $num2, $option="develop") {
+if($option == "test_bool") {
+  if($num1+$num2 == 90) {
+   return True;    
+    } else {
+   return False;
+   }
+} else if($option == "test_result") {
+  return $num1+$num2;
+} else {
+  if($num1+$num2 == 90) {
+   echo "Summation of 2 Number is 90";
+   return True;    
+    } else {
+   echo "Summation of 2 Number is not 90";
+   return False;
+   }
+}
+}
+
+// Check Number 1 and number 2
+if(isset($_POST["number1"]) AND isset($_POST["number2"]) ) {
+  $number1 = (int)$_POST["number1"];
+$number2 = (int)$_POST["number2"];
+  sum($number1, $number2);
+} else {
+  echo "Please Enter Number 1 and Number 2";
+}
+
+?>
+</body>
+</html>
---
+++ week2_homework/sum_unittest.php
@@ -0,0 +1,28 @@
+﻿<?php
+require_once("index.php");
+use PHPUnit\Framework\TestCase;
+
+class SumTestCase extends TestCase
+{
+    public function test_tc1_expected_90()
+    {
+        echo "Test Case 1: Add Number to 90";
+        $this->assertEquals(sum(90, 0, "test_result"), 90);
+        $this->assertEquals(sum(45, 45, "test_result"), 90);
+        $this->assertEquals(sum(40, 50, "test_result"), 90);
+        $this->assertEquals(sum(80, 10, "test_result"), 90);
+        $this->assertEquals(sum(15, 75, "test_result"), 90);
+    }
+
+    public function test_tc2_expected_bool()
+    {
+        echo "Test Case 2: Return True if summary is 90";
+        $this->assertTrue(sum(90, 0, "test_bool"));
+        $this->assertTrue(sum(45, 45, "test_bool"));
+        $this->assertFalse(sum(89, 2, "test_bool"));
+        $this->assertFalse(sum(66, 32, "test_bool"));
+        $this->assertFalse(sum(39, 367, "test_bool"));
+    }
+}
+
+?>
